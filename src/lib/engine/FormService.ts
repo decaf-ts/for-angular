8b1fcfc74273daaa188460fd052487be77b3f2ab
Tiago <venceslau.tiago@gmail.com>
2024-11-29 11:42:04 +0000

    DECAF-83 setup rendering engine. basic test passing 16h 32m
    Signed-off-by: Tiago <venceslau.tiago@gmail.com>

--- src/lib/engine/FormService.ts
+++ src/lib/engine/FormService.ts
@@ -1,11 +1,20 @@
 import {
+  CrudOperationKeys,
   FieldProperties,
   HTML5InputTypes,
   ValidatableByType,
 } from '@decaf-ts/ui-decorators';
 import { AngularFieldDefinition } from './types';
-import { FormControl, FormGroup } from '@angular/forms';
-import { ValidationKeys } from '@decaf-ts/decorator-validation';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ValidatorFn,
+} from '@angular/forms';
+import { ValidationKeys, validator } from '@decaf-ts/decorator-validation';
+import { Validation } from '@decaf-ts/decorator-validation';
+import { UnsupportedError } from '@decaf-ts/core';
+import { InternalError } from '@decaf-ts/db-decorators';
 
 export class FormService {
   static fromProps(props: FieldProperties & AngularFieldDefinition): FormGroup {
@@ -20,12 +29,17 @@ export class FormService {
     return new FormGroup(controls);
   }
 
-  private static validatorFor(key: string) {
+  private static validatorFor(key: string): ValidatorFn {
     if (!(key in Object.values(ValidationKeys))) {
       throw new Error('Unsupported custom validation');
     }
 
-    return;
+    return (control: AbstractControl) => {
+      const validator = Validation.get(key);
+      if (!validator) throw new InternalError(`No Validator found for key`);
+      const err = validator.hasErrors(control.value);
+      return null;
+    };
   }
 
   private static validatorsFromProps(
