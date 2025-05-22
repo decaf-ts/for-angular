4a26b5046786f1efb681dc15bb86e4398283840b
Pedro Costa <pcesar_costa@hotmail.com>
2025-05-22 11:07:43 -0300

    DECAF-101 Update ValidatorFactory

--- src/lib/engine/ValidatorFactory.ts
+++ src/lib/engine/ValidatorFactory.ts
@@ -1,19 +1,32 @@
 import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
-import { ComparisonValidationKeys, Validation, Validator } from '@decaf-ts/decorator-validation';
+import {
+  ComparisonValidationKeys,
+  Validation,
+  Validator,
+} from '@decaf-ts/decorator-validation';
 import { FieldProperties, parseValueByType } from '@decaf-ts/ui-decorators';
 import { NgxFormService } from './NgxFormService';
 
 export class ValidatorFactory {
-  static spawn(fieldProps: FieldProperties, key: string, formId: string): ValidatorFn {
-    if (!Validation.keys().includes(key)) throw new Error('Unsupported custom validation');
+  static spawn(
+    fieldProps: FieldProperties,
+    key: string,
+    formId: string
+  ): ValidatorFn {
+    if (!Validation.keys().includes(key))
+      throw new Error('Unsupported custom validation');
 
     const type = fieldProps.type;
 
-    const validatorFn: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+    const validatorFn: ValidatorFn = (
+      control: AbstractControl
+    ): ValidationErrors | null => {
       const validator = Validation.get(key) as Validator;
 
       const value =
-        typeof control.value !== 'undefined' ? parseValueByType(type, control.value, fieldProps) : undefined;
+        typeof control.value !== 'undefined'
+          ? parseValueByType(type, control.value, fieldProps)
+          : undefined;
 
       // arg = RenderingEngine.get().translate(arg as string, false);
       // const actualArg = parseArgs(arg);
@@ -22,15 +35,19 @@ export class ValidatorFactory {
       if (Object.values(ComparisonValidationKeys).includes(key as any)) {
         try {
           formData = NgxFormService.getFormData(formId);
-        } catch (e: any) {
-        }
+          console.log('formData=', formData);
+        } catch (e: any) {}
       }
 
       let errs;
       try {
-        errs = validator.hasErrors(value, {
-          [key]: fieldProps[key as keyof FieldProperties],
-        }, formData);
+        errs = validator.hasErrors(
+          value,
+          {
+            [key]: fieldProps[key as keyof FieldProperties],
+          },
+          formData
+        );
       } catch (e: unknown) {
         console.warn(`${key} validator failed to validate: ${e}`);
       }
