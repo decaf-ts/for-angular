73239d2734631d8ee2b49611f4c5c39d245d713f
Pedro Costa <pcesar_costa@hotmail.com>
2025-06-30 14:46:27 -0300

    DECAF-148 Add jest coverage

---
+++ workdocs/reports/jest.coverage.config.ts
@@ -0,0 +1,43 @@
+import { Config } from '@jest/types';
+import conf from '../../jest.config';
+
+const config: Config.InitialOptions = {
+  ...conf,
+  rootDir: '../..',
+  collectCoverage: true,
+  coverageDirectory: './workdocs/reports/coverage',
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        outputDirectory: './workdocs/reports/junit',
+        outputName: 'junit-report.xml',
+      },
+    ],
+    [
+      'jest-html-reporters',
+      {
+        publicPath: './workdocs/reports/html',
+        filename: 'test-report.html',
+        openReport: true,
+        expand: true,
+        pageTitle: 'ui-decorators Test Report',
+        stripSkippedTest: true,
+        darkTheme: true,
+        enableMergeData: true,
+        dataMergeLevel: 2,
+      },
+    ],
+  ],
+  coverageThreshold: {
+    global: {
+      statements: 25,
+      branches: 10,
+      functions: 15,
+      lines: 25,
+    },
+  },
+};
+
+export default config;
