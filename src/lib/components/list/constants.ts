6f21a8bea8c8a409772e656c49aaddf70d06ce67
tvenceslau <venceslau.tiago@gmail.com>
2025-09-15 02:44:41 +0100

    DECAF-185 - refactored FabricClientAdapter, updated dependencies, and optimized NgxBaseComponent code`

--- src/lib/components/list/constants.ts
+++ src/lib/components/list/constants.ts
@@ -1,10 +1,10 @@
-import { Adapter, Repository } from "@decaf-ts/core";
-import { Context, RepositoryFlags } from "@decaf-ts/db-decorators";
-import { Constructor, Model } from "@decaf-ts/decorator-validation";
+import { Adapter, Repository } from '@decaf-ts/core';
+import { Context, RepositoryFlags } from '@decaf-ts/db-decorators';
+import { Constructor, Model } from '@decaf-ts/decorator-validation';
 
 export enum ListComponentsTypes {
   INFINITE = 'infinite',
-  PAGINATED = 'paginated'
+  PAGINATED = 'paginated',
 }
 
 export interface IListEmptyResult {
@@ -17,16 +17,24 @@ export interface IListEmptyResult {
 }
 
 export interface RawQuery<M extends Model> {
-    select: undefined | (keyof M)[];
-    from: Constructor<M>;
-    where: (el: M) => boolean;
-    sort?: (el: M, el2: M) => number;
-    limit?: number;
-    skip?: number;
+  select: undefined | (keyof M)[];
+  from: Constructor<M>;
+  where: (el: M) => boolean;
+  sort?: (el: M, el2: M) => number;
+  limit?: number;
+  skip?: number;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type DecafRepositoryAdapter = Adapter<Map<string, Map<string | number, any>>, RawQuery<any>, RepositoryFlags & {UUIID?: string}, Context<RepositoryFlags & {UUIID?: string}>>
+export type DecafRepositoryAdapter<
+  F extends RepositoryFlags = RepositoryFlags,
+  C extends Context<F> = Context<F>
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+> = Adapter<any, any, RawQuery<any>, F, C>;
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type DecafRepository<M extends Model> = Repository<M, RawQuery<any>, DecafRepositoryAdapter, RepositoryFlags, Context<RepositoryFlags>>;
+export type DecafRepository<M extends Model> = Repository<
+  M,
+  RawQuery<M>,
+  DecafRepositoryAdapter<RepositoryFlags, Context<RepositoryFlags>>,
+  RepositoryFlags,
+  Context<RepositoryFlags>
+>;
