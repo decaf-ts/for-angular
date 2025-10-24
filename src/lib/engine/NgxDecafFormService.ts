0f57a00725b6e236fa01c6c3dddef0e8ec0c7729
Demerson Carvalho <demersonmcarvalho@hotmail.com>
2025-10-24 19:45:37 -0300

    DECAF-26 - fix

--- src/lib/engine/NgxDecafFormService.ts
+++ src/lib/engine/NgxDecafFormService.ts
@@ -222,7 +222,7 @@ export class NgxDecafFormService {
     for (const part of parts) {
       if (!currentGroup.get(part)) {
         const partFormGroup = (isMultiple && part === childOf) ? new FormArray([new FormGroup({})]) : new FormGroup({});
-        let pk = componentProps?.pk || parentProps?.pk || '';
+        const pk = componentProps?.pk || parentProps?.pk || '';
         (partFormGroup as KeyValue)[BaseComponentProps.FORM_GROUP_COMPONENT_PROPS] = {
           childOf: childOf || '',
           isMultiple: isMultiple,
