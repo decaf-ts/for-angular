dc21c61b990d63a602c01c951e73c99caeca18ea
tvenceslau <venceslau.tiago@gmail.com>
2026-02-25 10:54:52 +0000

    patch - DECAF-213 update-no-ci - after release preparation

--- bin/tag-release.sh
+++ bin/tag-release.sh
@@ -137,5 +137,5 @@ fi
 
 if [[ "$MESSAGE" =~ -no-ci$ ]]; then
   # Use .npmtoken for publishing; respect chosen access level
-  NPM_TOKEN=$(cat .npmtoken) npm publish --access "$NPM_ACCESS_VALUE"
+  NPM_TOKEN=$(cat .npmtoken) npm publish ./dist/lib --access "$NPM_ACCESS_VALUE"
 fi
