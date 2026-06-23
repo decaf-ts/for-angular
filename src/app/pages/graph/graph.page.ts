fdf10b9345ec9d9f6648383f01d2050edf1f7598
demersonmcarvalho <demersonmcarvalho@hotmail.com>
2026-06-22 21:26:19 -0300

    DECAF-26 - graph component update

--- src/app/pages/graph/graph.page.ts
+++ src/app/pages/graph/graph.page.ts
@@ -1,18 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonContent } from '@ionic/angular/standalone';
 import { GraphRendererComponent } from 'src/graph';
+import { NgxPageDirective } from 'src/lib/engine';
 import { GraphPublishingWorkflow } from './workflow-root';
 
 @Component({
   selector: 'app-graph',
   standalone: true,
-  imports: [
-    IonContent,
-    GraphRendererComponent,
-  ],
+  imports: [IonContent, GraphRendererComponent],
   templateUrl: './graph.page.html',
   styleUrl: './graph.page.scss',
 })
-export class GraphPage {
+export class GraphPage extends NgxPageDirective implements OnInit {
   readonly workflowRoot = GraphPublishingWorkflow;
+
+  constructor() {
+    super('GraphPage', false);
+  }
+
+  async ngOnInit(): Promise<void> {}
 }
